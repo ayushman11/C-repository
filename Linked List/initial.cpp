ca6b118cd32da2479ddd86ce02591e08d4c60ab0
Ayushman <ayushman11022002@gmail.com>
2021-11-04 14:18:23 +0530

    initial

---
+++ Linked List/initial.cpp
@@ -0,0 +1,112 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class node {
+    public:
+    int data;
+    node* next;
+
+    node(int val)
+    {
+        data = val;
+        next = NULL;
+    }
+};
+
+void insertAtHead(node* &head, int val)
+{
+    node* n = new node(val);
+    n->next = head;
+    head = n;
+    return;
+}
+
+void insertAtTail(node* &head, int val)
+{
+    node* n = new node(val);
+    if(head == NULL)
+    {
+        head = n;
+        return;
+    }
+    node* temp = head;
+
+    while(temp->next != NULL)
+    {
+        temp= temp->next;
+    }
+    temp->next = n;
+    return;
+}
+
+void display(node* head)
+{
+    node* temp = head;
+    while(temp != NULL)
+    {
+        cout<<temp->data<<"->";
+        temp = temp->next;
+
+    }
+    cout<<"NULL"<<endl;
+    return;
+}
+
+bool isPresent(node* head, int key)
+{
+    node* temp = head;
+    while(temp != NULL)
+    {
+        if(temp->data == key) return true;
+        temp = temp->next;
+    }
+    return false;
+}
+
+void deleteAtHead(node* &head)
+{
+    node* todelete = head;
+    head = head->next;
+    delete todelete;
+}
+
+void deletion(node* &head, int key)
+{
+    if(head == NULL)
+    {
+        return;
+    }
+
+    if(head->next == NULL)
+    {
+        deleteAtHead(head);
+        return;
+    }
+    node* temp = head;
+
+    while(temp->next->data != key)
+    {
+        temp = temp->next;
+    }
+
+    node* todelete = temp->next;
+    temp->next = temp->next->next;
+    delete todelete;
+}
+
+int main()
+{
+    node* head = NULL;
+    insertAtTail(head,1);
+    insertAtTail(head,2);
+    insertAtTail(head,3);
+    display(head);
+    insertAtHead(head,4);
+    display(head);
+    cout<<isPresent(head,4)<<endl;
+    deletion(head,3);
+    display(head);
+    deleteAtHead(head);
+    display(head);
+
+}
